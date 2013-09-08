e710d4d399a1e0b45ac0a51f5db47ddefe9aee89
Richard Aberefa <ngrichyj4@gmail.com>
2013-09-08 14:19:02 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Velvet::Application.config.secret_token = 'd5975e81f4a4c3c8c09a1d5b12940cb38fb2a13667a82a8fc1c196055aae7b7e790c8b6f15383cc458169b840bddc5619f0350ea5496dbb81750b1c312093491'
