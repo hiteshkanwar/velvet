042ea54786320231b4ba8ee6abf7783b22c5604a
Amar Daxini <amardaxini@gmail.com>
2014-02-03 19:56:51 +0530

    revert back redis config from dev

--- config/initializers/captcha.rb
+++ config/initializers/captcha.rb
@@ -1,7 +1,7 @@
 
-# uri = URI.parse(ENV["REDISCLOUD_URL"])
-# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-REDIS = Redis.new
+uri = URI.parse(ENV["REDISCLOUD_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# REDIS = Redis.new
 redis = REDIS
 
 redis = Redis::Namespace.new('express:captcha', redis: redis)
