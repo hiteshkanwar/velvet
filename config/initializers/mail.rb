43d8650d07537ddcfb77ee96330e95a9232d576b
HTP <htp@htp-desktop.(none)>
2014-03-10 11:16:14 +0530

    chnages on mailer

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :domain               => 'gmail',
-   :user_name            => 'prateekyuvasoft101@gmail.com',
-   :password             => 'yuvasoft2013',
-   :authentication       => 'plain',
-   :enable_starttls_auto => true 
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method ||= :smtp
