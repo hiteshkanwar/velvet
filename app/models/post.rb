a9dcca9231ffaca503b1ed11fd90e375b6cbda68
HTP <htp@htp-desktop.(none)>
2014-03-13 17:15:06 +0530

    add video functionality pornhub redtube and youporn

--- app/models/post.rb
+++ app/models/post.rb
@@ -32,6 +32,7 @@ class Post < ActiveRecord::Base
     
     media_regex = /(?:.be\/|\/watch\?v=|\/(?=p\/))([\w\/\-]+)/
     media_urls = self.body.to_s.scan(media_regex).flatten
+    
     if !media_urls.blank?  
       media_urls.each do |youtube|
         self.media = Media.create(name: "YouTube", url: "//www.youtube.com/embed/#{youtube}")
@@ -41,11 +42,29 @@ class Post < ActiveRecord::Base
     media1 = /(?:http:\/\/)?(?:www\.)?(?:youporn\.com)\/(?:watch)?(.+)/
     media_urls = self.body.to_s.scan(media1).flatten
     
-    media_urls.each do |youtube|
-
-        self.media = Media.create(name: "Youporn", url: "//www.youporn.com/embed#{youtube}")
+    if !media_urls.blank?  
+      media_urls.each do |youtube|
+        self.media = Media.create(name: "YouPorn", url: "//www.youporn.com/embed#{youtube}")
         break
       end
+    end 
+    media2 = /(?:http:\/\/)?(?:www\.)?(?:redtube\.com)\/(?:watch)?(.+)/
+    media_urls = self.body.to_s.scan(media2).flatten
+    if !media_urls.blank? 
+      media_urls.each do |youtube|
+        self.media = Media.create(name: "RedTube", url: "//embed.redtube.com/player/?id=#{youtube}&style=redtube")
+        break
+      end
+    end
+    media3 = /(?:http:\/\/)?(?:www\.)?(?:pornhub\.com)\/(?:watch)?(.+)/
+    media_urls = self.body.to_s.scan(media3).flatten
+    if !media_urls.blank? 
+      media_urls.each do |youtube|
+        youtube= youtube.gsub('view_video.php?viewkey=','')
+        self.media = Media.create(name: "PornHub", url: "//www.pornhub.com/embed/#{youtube}")
+        break
+      end
+    end
   end
 
 end
